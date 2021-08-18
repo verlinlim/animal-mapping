7ef46d66d43805857bf81e0327cdc326cc81c196
verlinlim <verlinlimhm@gmail.com>
2021-08-18 22:58:03 +0800

    INITIAL COMMIT

---
+++ animal-mapping/src/main/java/com/animalmapping/AnimalMappingApplication.java
@@ -0,0 +1,13 @@
+package com.animalmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalMappingApplication.class, args);
+	}
+
+}
